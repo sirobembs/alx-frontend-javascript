1af3fc4db8ca9f6e1dc5651a32c3c2f0fd4a9e2b
Daniel Obembe <danielobembe1@gmail.com>
2022-12-23 02:37:45 +0100

    cross my fingers

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+
+  const result = numerator / denominator;
+
+  return result;
+}
