77d34967d914a5a80aa0a0c716c1ca8f3d5b9ae6
Daniel Obembe <danielobembe1@gmail.com>
2022-12-23 03:58:13 +0100

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -8,8 +8,5 @@ export default class EVCar extends Car {
   }
 
   cloneCar() {
-    const NewObj = this.constructor[Symbol.species] || this.constructor;
-    const clone = new NewObj();
-    return clone;
-  }
+    return new Car();
 }
