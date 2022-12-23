5a74bbae48b759f1d2a17b20a9c05d7e5da6ba17
Daniel Obembe <danielobembe1@gmail.com>
2022-12-23 01:56:18 +0100

    dont make a promise you cant keep

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,12 @@
-function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
-    .catch(() => Error())
-    .finally(() => console.log('Got a response from the API'));
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
-
-export default handleResponseFromAPI;
