16c1f62eadf241acdc7f9bc9defcd43d04cdb6a5
Daniel Obembe <danielobembe1@gmail.com>
2022-12-23 01:43:57 +0100

    made some corretions to 0-make promise you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,14 @@
-// Only make Promise
+// Keep every promise you make and only make promise you can keep
+
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
+    /* eslint-disable */
     if (true) {
       resolve();
     } else {
       reject();
     }
+    /* eslint-enable */
   });
 }
 
