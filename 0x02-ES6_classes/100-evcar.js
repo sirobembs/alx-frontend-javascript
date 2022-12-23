e033530766e960a10dac3b57b91b667bb8695840
Daniel Obembe <danielobembe1@gmail.com>
2022-12-23 04:00:20 +0100

    EVCar again

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,12 +1,14 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
 
-    this._range = range;
-  }
+		this._range = range;
+	}
 
-  cloneCar() {
-    return new Car();
+	cloneCar() {
+		const NewObj = this.constructor[Symbol.species] || this.constructor;
+		const clone = new NewObj();
+		return clone;
 }
