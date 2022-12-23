358b9c79b4f627820f20ba899b50dd9c055cb843
Daniel Obembe <danielobembe1@gmail.com>
2022-12-23 01:40:41 +0100

    finally finished promise task

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+// Only make Promise
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
+
+export default getResponseFromAPI;
